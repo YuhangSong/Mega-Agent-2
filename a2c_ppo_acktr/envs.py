a9f5667b8165217a5e5287bd4bc936b91f5bce78
YuhangSong <YuhangSong2017@gmail.com>
2019-01-15 22:35:02 +0800

    change position of wrapper extra time limit

--- a2c_ppo_acktr/envs.py
+++ a2c_ppo_acktr/envs.py
@@ -70,6 +70,11 @@ def make_env(env_id, seed, rank, log_dir, add_timestep, allow_early_resets, crop
             env.unwrapped, gym.envs.atari.atari_env.AtariEnv)
         if is_atari:
             env = make_atari(env_id)
+            if env_id in ['PongNoFrameskip-v4']:
+                env = ExtraTimeLimit(
+                    env = env,
+                    max_episode_steps=int(4500/4),
+                )
 
         env.seed(seed + rank)
 
@@ -88,11 +93,6 @@ def make_env(env_id, seed, rank, log_dir, add_timestep, allow_early_resets, crop
                 env = wrap_deepmind(env)
                 if crop_obs is not None:
                     env = CropFrame(env,crop_obs)
-                if env_id in ['PongNoFrameskip-v4']:
-                    env = ExtraTimeLimit(
-                        env = env,
-                        max_episode_steps=int(4500/4),
-                    )
         elif len(env.observation_space.shape) == 3:
             raise NotImplementedError("CNN models work only for atari,\n"
                 "please use a custom wrapper for a custom pixel input env.\n"
